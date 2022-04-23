76b35caf01c6a1fedabaeb80daf9969c262598c3
Negin Soleimani <negin.kdk1@gmail.com>
2022-04-23 14:59:46 +0430

    Create background stars

--- index.js
+++ index.js
@@ -67,12 +67,13 @@ class Projectile {
 }
 
 class Particle {
-    constructor({ position, velocity, radius, color}) {
+    constructor({ position, velocity, radius, color, fades }) {
         this.position = position
         this.velocity = velocity
         this.radius = radius
         this.color = color
         this.opacity = 1
+        this.fades = fades
     }
 
     draw() {
@@ -90,7 +91,7 @@ class Particle {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        this.opacity-=0.01
+        if (this.fades) this.opacity -= 0.01
     }
 }
 
@@ -238,7 +239,23 @@ const keys = {
 let frames = 0
 let randomInterval = Math.floor(Math.random() * 500 + 500)
 
-function createParticles({object,color}){
+
+for (let i = 0; i < 100; i++) {
+    particles.push(new Particle({
+        position: {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height
+        },
+        velocity: {
+            x: 0,
+            y: 0.3
+        },
+        radius: Math.random() * 1,
+        color: '#fff'
+    }))
+}
+
+function createParticles({ object, color,fades }) {
     for (let i = 0; i < 10; i++) {
         particles.push(new Particle({
             position: {
@@ -250,7 +267,8 @@ function createParticles({object,color}){
                 y: (Math.random() - 0.5) * 4
             },
             radius: Math.random() * 3,
-            color:color || '#9400D3'
+            color: color || '#9400D3',
+            fades
         }))
     }
 }
@@ -260,12 +278,19 @@ function animate() {
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
-    particles.forEach((particle , i) => {
-        if(particle.opacity<=0){
-            setTimeout(()=>{
-                particles.splice(i,1)
-            },0)
-        }else{
+    particles.forEach((particle, i) => {
+
+        if(particle.position.y -particle.radius >= canvas.height){
+            particle.position.x= Math.random()*canvas.width
+            particle.position.y= -particle.radius
+        }
+
+
+        if (particle.opacity <= 0) {
+            setTimeout(() => {
+                particles.splice(i, 1)
+            }, 0)
+        } else {
             particle.update()
         }
     })
@@ -280,13 +305,14 @@ function animate() {
         if (invaderProjectile.position.y + invaderProjectile.height >= player.position.y
             && invaderProjectile.position.x + invaderProjectile.width >= player.position.x &&
             invaderProjectile.position.x <= player.position.x + player.width) {
-                setTimeout(() => {
-                    invaderProjectiles.splice(index, 1)
-                }, 0)
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1)
+            }, 0)
             console.log("lose")
             createParticles({
-                object:player,
-                color:'#fff'
+                object: player,
+                color: '#fff',
+                fades:true
             })
         }
     })
@@ -324,7 +350,8 @@ function animate() {
                         const projectileFound = projectiles.find(projectile2 => projectile2 === projectile)
                         if (invaderFound && projectileFound) {
                             createParticles({
-                                object:invader,
+                                object: invader,
+                                fades:true
                             })
                             grid.invaders.splice(i, 1)
                             projectiles.splice(j, 1)
